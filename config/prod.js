14bef3eca43595765f519241e2669cf18fecc4ad
nik32 <51018572+nik32@users.noreply.github.com>
2021-02-21 22:12:51 +0530

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,5 @@
 //We store very important and sensitive data in our servers envirenment variable as it is safer. Becuase even if somebdy views our code, he won't be able to get the sensitive info
 module.exports = {
     mongoURL : process.env.mongoURL,
-    jwt_secret_key: process.env.jwt_secret_key,
-    firebase_config: process.env.firebase_config,
+    jwt_secret_key: process.env.jwt_secret_key
 }
