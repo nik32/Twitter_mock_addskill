2035e425be4dc1056dc6cce1562147988df93ed1
nik32 <nikhil12509818@gmail.com>
2021-02-22 04:45:39 +0530

    updated the path of env_sample

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURL : "mongodb+srv://admin:admin@cluster0.hsate.mongodb.net/socail_media?retryWrites=true&w=majority", 
-    jwt_secret_key : "This is my secret Key: 99482uruiw47iwyiwr99824",
-}
