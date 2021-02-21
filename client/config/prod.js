effe15e8d06792885bef95dcfa8c272788ad0536
nik32 <51018572+nik32@users.noreply.github.com>
2021-02-21 22:16:28 +0530

    Add files via upload

---
+++ client/config/prod.js
@@ -0,0 +1,4 @@
+//We store very important and sensitive data in our servers envirenment variable as it is safer. Becuase even if somebdy views our code, he won't be able to get the sensitive info
+module.exports = {
+    firebase_config: process.env.firebase_config,
+}
