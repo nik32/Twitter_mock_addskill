f3aaa3511e8c3602e748195301fbcd9393e49d1a
nik32 <51018572+nik32@users.noreply.github.com>
2021-02-22 03:25:42 +0530

    Update firebase.js

--- client/src/misc/firebase.js
+++ client/src/misc/firebase.js
@@ -3,12 +3,9 @@ import firebase from 'firebase/app';//This line is a must, to use firebase as gi
 import 'firebase/storage';//From this point we should import the perticular services we want to use
 
 //firebase - Becasue had the blaze plan already, so scalabilty - if needed, will not be a problem as won't have to pay a fixed amount every month (like in cloudinary). It will automatically charge as per the app usge. And even cheaper than cloudniary 
-import {firebase_config} from '../config/keys' ;
-console.log(firebase_config);
-console.log(process.env.REACT_APP_TEST)
 
   // Initialize Firebase
-  firebase.initializeApp(firebase_config);
+  firebase.initializeApp(process.env.REACT_APP_FIREBASE_CONFIG);
 
   const storage = firebase.storage();
 
