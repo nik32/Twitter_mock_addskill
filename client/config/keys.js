7726f3577bc20966285870565604cf2cc12817bb
nik32 <51018572+nik32@users.noreply.github.com>
2021-02-21 22:20:26 +0530

    Delete client/config directory

--- client/config/keys.js
+++
@@ -1,5 +0,0 @@
-//We store very important and sensitive data in our servers envirenment variable as it is safer. Becuase even if somebdy views our code, he won't be able to get the sensitive info
-if(process.env.NODE_ENV == 'production')
-    module.exports = require("./prod");
-else
-    module.exports = require("./dev");
