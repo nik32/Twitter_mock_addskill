214c2c4b782c9b09c00f8a606612202f5f800c34
nik32 <51018572+nik32@users.noreply.github.com>
2021-02-22 02:54:53 +0530

    Update firebase.js

--- client/src/misc/firebase.js
+++ client/src/misc/firebase.js
@@ -5,7 +5,7 @@ import 'firebase/storage';//From this point we should import the perticular serv
 //firebase - Becasue had the blaze plan already, so scalabilty - if needed, will not be a problem as won't have to pay a fixed amount every month (like in cloudinary). It will automatically charge as per the app usge. And even cheaper than cloudniary 
 import {firebase_config} from '../config/keys' ;
 console.log(firebase_config);
-console.log(process.env.Test)
+console.log(process.env.REACT_APP_TEST)
 
   // Initialize Firebase
   firebase.initializeApp(firebase_config);
